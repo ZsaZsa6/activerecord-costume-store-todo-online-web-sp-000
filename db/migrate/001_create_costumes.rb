71dd38be7aee8dadc232c1399ac1c838d6fd5d29
Ronda D Cowen <cowenhome5@gmail.com>
2020-03-25 22:05:34 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,8 +4,8 @@ def change
   create_table :costumes do |t|
     t.string :name
     t.float :price
-    t.integer :size
-    t.text :image_url
+    t.string :size
+    t.url :image_url
     t.timestamps
     end
   end
